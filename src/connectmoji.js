d011bbe5689a110f8ab2ef7537be77b46cc6f2c5
Robert Chen <chen1fan@icloud.com>
2022-01-31 21:18:12 -0500

    add generate board function

--- src/connectmoji.js
+++ src/connectmoji.js
@@ -1,2 +1,30 @@
 // implement your functions here
 // ...don't forget to export functions!
+function generateBoard(rows, cols, fill){
+ 
+    if (fill === undefined) {
+        fill = null;
+    }
+
+    let array = Array(0);
+
+    for(let i = rows*cols; i > 0 ; i--){
+
+        array.push(fill);
+    }
+
+    let board = {data: array, rows: rows, cols: cols};
+
+    return board;
+}
+
+
+module.exports = {
+
+generateBoard: generateBoard,
+
+
+}
+
+
+
